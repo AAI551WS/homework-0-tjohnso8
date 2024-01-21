5eddee791d8539c19c479151b59b66548bb8593e
tjohnso8 <112779755+tjohnso8@users.noreply.github.com>
2024-01-20 23:36:43 -0500

    added lines to print hello world and add variables

--- answer.py
+++ answer.py
@@ -1,3 +1,4 @@
+#Last updated by Tyler Johnson
 def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
@@ -8,10 +9,13 @@ def hello_world():
     # Write a comment; comments are very useful to other programmers.
 
     # Write a Python statement to print Hello, World!
-
+    
+    #The line below will simply print "Hello World" to the console
+    print("Hello World")
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    #The following lines will add the variables a and b and store to the output variable
+    output = a + b
 
     return output

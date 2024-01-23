ae259f007f00371c377321c466daac150c2e344f
tjohnso8 <112779755+tjohnso8@users.noreply.github.com>
2024-01-23 17:03:37 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -3,7 +3,7 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+    This function will print "Hello World"
     """
 
     # Write a comment; comments are very useful to other programmers.
